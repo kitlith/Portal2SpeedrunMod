f8c844eccbf0dfcd9cda6ffb444adbe681d7d5e9
Kitlith <kitlith@kitl.pw>
2020-04-25 12:48:52 -0700

    Port Linux offsets from SAR.
    Unfortunately, smsm has some offsets that SAR doesn't, so it's
    incomplete. I've left some placeholders for the offsets that are
    currently missing.

--- smsm/src/Portal2.cpp
+++ smsm/src/Portal2.cpp
@@ -11,6 +11,7 @@ void Portal2::LoadOffsets()
 {
     using namespace Offsets;
 
+#ifdef _WIN32
     // engine.dll
 
     InternalSetValue = 12; // ConVar
@@ -100,12 +101,113 @@ void Portal2::LoadOffsets()
     DrawTexturedRect = 39; // CMatSystemSurface
     IsTextureIDValid = 40; // CMatSystemSurface
     CreateNewTextureID = 41; // CMatSystemSurface
+#else
+    // engine.so
+
+    InternalSetValue = 19; // ConVar
+    InternalSetFloatValue = 20; // ConVar
+    InternalSetIntValue = 21; // ConVar
+    ClientCmd = 7; // CEngineClient
+    GetClientStateFunction = 11; // CEngineClient::ClientCmd
+    Cbuf_AddText = 45; // CEngineClient::ClientCmd
+    s_CommandBuffer = 69; // Cbuf_AddText
+    CCommandBufferSize = 9556; // Cbuf_AddText
+    m_bWaitEnabled = 8265; // CCommandBuffer::AddText
+    GetActiveSplitScreenPlayerSlot = 127; // CEngineClient
+    SetSignonState = 3; // CDemoRecorder
+    HostState_OnClientConnected = 735; // CClientState::SetSignonState
+    hoststate = 9; // HostState_OnClientConnected
+    AutoCompletionFunc = 37; // listdemo_CompletionFunc
+    ClientCommand = 39; // CVEngineServer
+    // TraceRay = ; // IEngineTrace
+    // NextParticleSystem = ;
+    ProcessMovement = 2; // CGameMovement
+    // m_fFlags = ; // CBaseEntity
+    // m_nTickBase = ; // CBaseEntity
+    // m_bDucking = ; // CBaseEntity
+    // m_hUseEntity = ; // CBaseEntity
+    GetScreenSize = 5; // CEngineClient
+    // PrecacheModel = ; // IEngineTool
+    // GetWorldToScreenMatrixForView = ; // IEngineTool
+
+    // client.so
+
+    GetHud = 104; // cc_leaderboard_enable
+    FindElement = 120; // cc_leaderboard_enable
+    // ChatPrintf = ; // CBaseHudChat
+    // RenderView = ;
+    HudUpdate = 11; // CHLClient
+
+    // server.so
+
+    // m_hActiveWeapon = ; // CBaseEntity
+    // m_bCanFirePortal1 = ; // CBaseEntity
+    UTIL_PlayerByIndex = 61; // CServerGameDLL::Think
+    gpGlobals = 12; // UTIL_PlayerByIndex
+    Think = 31; // CServerGameDLL
+    GetIServerEntity = 2; // CServerTools
+    m_EntPtrArray = 48; // CServerTools::GetIServerEntity
+    // CBaseEntityActivate = ;
+    // CBaseEntitySpawn = ;
+
+    // libvstdlib.so
+
+    RegisterConCommand = 9; // CCVar
+    UnregisterConCommand = 10; // CCvar
+    FindCommandBase = 13; // CCvar
+    m_pConCommandList = 48; // CCvar
+
+    // vscript.so
+
+    // CreateVM = ;
+
+    // vgui2.so
+
+    GetIScheme = 9; // CSchemeManager
+    GetFont = 4; // CScheme
+
+    // vguimatsurface.so
+
+    DrawSetColor = 13; // CMatSystemSurface
+    DrawFilledRect = 15; // CMatSystemSurface
+    DrawLine = 18; // CMatSystemSurface
+    DrawSetTextFont = 22; // CMatSystemSurface
+    DrawSetTextColor = 24; // CMatSystemSurface
+    GetFontTall = 72; // CMatSystemSurface
+    PaintTraverseEx = 117; // CMatSystemSurface
+    StartDrawing = 193; // CMatSystemSurface::PaintTraverseEx
+    FinishDrawing = 590; // CMatSystemSurface::PaintTraverseEx
+    DrawColoredText = 160; // CMatSystemSurface
+    DrawTextLen = 163; // CMatSystemSurface
+    // DisableClipping = ; // CMatSystemSurface
+    Paint = 15; // CEngineVGui
+
+    // DrawGetTextureId = ; // CMatSystemSurface
+    // DrawGetTextureFile = ; // CMatSystemSurface
+    // DrawSetTextureFile = ; // CMatSystemSurface
+    // DrawSetTextureRGBA = ; // CMatSystemSurface
+    // DrawSetTexture = ; // CMatSystemSurface
+    // DrawGetTextureSize = ; // CMatSystemSurface
+    // DrawTexturedRect = ; // CMatSystemSurface
+    // IsTextureIDValid = ; // CMatSystemSurface
+    // CreateNewTextureID = ; // CMatSystemSurface
+#endif
 }
+
 const char* Portal2::Version()
 {
+#ifdef _WIN32
     return "Portal 2 (7054)";
+#else
+    return "Portal 2 (7293)";
+#endif
 }
+
 const char* Portal2::Process()
 {
+#ifdef _WIN32
     return "portal2.exe";
+#else
+    return "portal2";
+#endif
 }
